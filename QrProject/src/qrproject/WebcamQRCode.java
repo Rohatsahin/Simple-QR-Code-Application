1e6fb89b3115306f066ab01db796e1d9d62d7fc9
Rohatsahin <rohatsahin92@gmail.com>
2015-09-30 00:35:45 +0300

    first commit

---
+++ QrProject/src/qrproject/WebcamQRCode.java
@@ -0,0 +1,110 @@
+
+package qrproject;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+
+public class WebcamQRCode extends JFrame implements Runnable, ThreadFactory {
+
+	private static final long serialVersionUID = 6441489157408381878L;
+
+	private Executor executor = Executors.newSingleThreadExecutor(this);
+
+	private Webcam webcam = null;
+	private WebcamPanel panel = null;
+	private JTextArea textarea = null;
+
+	public WebcamQRCode() {
+		super();
+
+		setLayout(new FlowLayout());
+		setTitle("Read QR With Webcam");
+		
+		Dimension size = new Dimension(640,480);
+
+		webcam = Webcam.getWebcams().get(0);
+		webcam.setViewSize(size);
+
+		panel = new WebcamPanel(webcam);
+		panel.setPreferredSize(size);
+
+		textarea = new JTextArea();
+		textarea.setEditable(false);
+                textarea.setFont(textarea.getFont().deriveFont(24f));
+		textarea.setPreferredSize(size);
+
+		add(panel);
+		add(textarea);
+
+		pack();
+		setVisible(true);
+
+		executor.execute(this);
+	}
+         
+
+	@Override
+	public void run() {
+
+		do {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			Result result = null;
+			BufferedImage image = null;
+
+			if (webcam.isOpen()) {
+
+				if ((image = webcam.getImage()) == null) {
+					continue;
+				}
+
+				LuminanceSource source = new BufferedImageLuminanceSource(image);
+				BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+				try {
+					result = new MultiFormatReader().decode(bitmap);
+				} catch (NotFoundException e) {
+					// fall thru, it means there is no QR code in image
+				}
+			}
+
+			if (result != null) {
+				textarea.setText(result.getText());
+			}
+
+		} while (true);
+	}
+
+	@Override
+	public Thread newThread(Runnable r) {
+		Thread t = new Thread(r, "example-runner");
+		t.setDaemon(true);
+		return t;
+	}
+
+	
+}
